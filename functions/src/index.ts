df4512bd28562d138a064b64417b1b55bd48d4ae
Tejus <tejusrevi@gmail.com>
2021-04-12 11:44:38 -0400

    Added cloud functions for GetNextTripsForStopAllRoutes & GetNextTripsForStop

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,38 @@
 import * as functions from "firebase-functions";
+import * as https from "https";
 
+export const GetNextTripsForStopAllRoutes = functions.https.onRequest((request, response) => {
+  if (request.query.stopNo == null){
+    response.status(400).send({"message":"Missing stop number"})
+  }
+  https.get(`https://api.octranspo1.com/v2.0/GetNextTripsForStopAllRoutes?appID=${functions.config().octracker.appid}&apiKey=${functions.config().octracker.apikey}&stopNo=${request.query.stopNo}`, (res)=>{
+    let rawData = '';  
+    res.on('data', (chunk) => { rawData += chunk; });
+    res.on('end', () => {
+        try {
+          const parsedData = JSON.parse(rawData);
+          response.send(parsedData);
+        } catch (e) {
+          response.status(400).send({"message":"OC Transpo server error"})
+        }
+      });
+    })
+ });
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-
+ export const GetNextTripsForStop = functions.https.onRequest((request, response) => {
+  if (request.query.stopNo == null || request.query.routeNo == null){
+    response.status(400).send({"message":"Missing stop or route number"})
+  }
+  https.get(`https://api.octranspo1.com/v2.0/GetNextTripsForStop?appID=${functions.config().octracker.appid}&apiKey=${functions.config().octracker.apikey}&stopNo=${request.query.stopNo}&routeNo=${request.query.routeNo}`, (res)=>{
+    let rawData = '';  
+    res.on('data', (chunk) => { rawData += chunk; });
+    res.on('end', () => {
+        try {
+          const parsedData = JSON.parse(rawData);
+          response.send(parsedData);
+        } catch (e) {
+          response.status(400).send({"message":"OC Transpo server error"})
+        }
+      });
+    })
  });
